4163f844fa087d6c3e2675c7021736b25b904aec
Shaik Saniya Anjum <shaiksaniyaanjum956@gmail.com>
2026-01-31 14:27:33 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,9 @@
+class Demo {
+public static void main(String args[])
+{
+    int a=15;
+    System.out.println(a);
+    a=20;
+    System.out.print(a);
+}   
+}
---
+++ Sum.java
@@ -0,0 +1,10 @@
+ class Sum {
+  public static void main(String args[])  
+  {
+    int a,b,c;
+    a=20;
+    b=30;
+    c=a+b;
+    System.out.println(c);
+  }
+}
